e16e0ae6d64b1cce120ca560d467ecb8799e9b8f
yjt2003 <118866582+yjt2003@users.noreply.github.com>
2023-02-06 16:31:44 +0800

    Add files via upload

---
+++ root_通用/通用.js
@@ -0,0 +1,42 @@
+var btn1 = document.getElementById("navOne");
+var btn2 = document.getElementById("navTwo");
+var btn3 = document.getElementById("navThree");
+var btn4 = document.getElementById("navFour");
+var btn5 = document.getElementById("navFive");
+btn1.addEventListener('click',()=>{
+  window.location.href="";
+})
+btn2.addEventListener('click',()=>{
+  window.location.href="";
+})
+btn3.addEventListener("click", () => {
+  window.location.href="";
+});
+btn4.addEventListener("click", () => {
+  window.location.href="";
+});
+btn5.addEventListener("click", () => {
+  window.location.href="";
+});
+search.addEventListener('click',()=>{//----------------------------------链接
+  alert(555);
+})
+about.addEventListener('click',()=>{//-------------------------------------链接
+  alert(555);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
